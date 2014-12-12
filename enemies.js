3a890fd7503b57c85c18344a8ab7fcfbe274361d
Heark <hearkofsorcerey@gmail.com>
2014-12-12 18:52:21 -0500

    Update enemies.js

--- enemies.js
+++ enemies.js
@@ -1,9 +1,3 @@
 // Enemies
-var Enemy = function(name, hp, att, def, spa){
-  this.name = name;
-  this.hp = hp;
-  this.att = att;
-  this.def = def;
-  this.spa = spa;
-}
+
 var slime = new Enemy("Slime", 35, 2, 2, 2);
