211e831815b8f9227af73e4cbfce2a462bcc3174
Heark <hearkofsorcerey@gmail.com>
2014-12-12 18:31:16 -0500

    Update characters.js

--- characters.js
+++ characters.js
@@ -5,13 +5,6 @@ var Player = function(hp, def, att, spa){
   this.att = att;
   this.spa = spa;
 }
-var Enemy = function(name, hp, att, def, spa){
-  this.name = name;
-  this.hp = hp;
-  this.att = att;
-  this.def = def;
-  this.spa = spa;
-}
 var Item = function(name, desc, effect, drop){
   this.name = name;
   this.desc = desc;
