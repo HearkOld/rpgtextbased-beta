b35660020169d7820d9136dddee9d5f3313e1332
Heark <hearkofsorcerey@gmail.com>
2014-12-12 11:40:23 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
 // Main
-/*
 
+var Player = function(hp, def, att, spa){
+  this.hp = hp;
+  this.def = def;
+  this.att = att;
+  this.spa = spa;
+}
+var Enemy = function(name, hp, att, def, spa){
+  this.name = name;
+  this.hp = hp;
+  this.att = att;
+  this.def = def;
+  this.spa = spa;
+}
+var Item = function(name, desc, effect, drop){
+  this.name = name;
+  this.desc = desc;
+  this.effect = effect;
+  this.drop = drop;
+}
 
-
-Testing
-*/
+// Player
+var player = new Player(225, 5, 5, 5);
+// Enemies
+var slime = new Enemy(35, 2, 2, 2);
+function startGame(); {
+var plname = prompt("What's your name pal?");
+document.write('Oh, hello '+name);
