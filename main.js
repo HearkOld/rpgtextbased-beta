3ea0a54ceb56addfed1217170677c554767f6316
Heark <hearkofsorcerey@gmail.com>
2014-12-12 11:52:12 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,4 @@
 // Main
-
-var Player = function(hp, def, att, spa){
-  this.hp = hp;
-  this.def = def;
-  this.att = att;
-  this.spa = spa;
-}
-var Enemy = function(name, hp, att, def, spa){
-  this.name = name;
-  this.hp = hp;
-  this.att = att;
-  this.def = def;
-  this.spa = spa;
-}
-var Item = function(name, desc, effect, drop){
-  this.name = name;
-  this.desc = desc;
-  this.effect = effect;
-  this.drop = drop;
-}
-
 // Player
 var player = new Player(225, 5, 5, 5);
 // Enemies
