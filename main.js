61f4c6564aff9313d6fb205cfca7889476ec37c1
Heark <hearkofsorcerey@gmail.com>
2014-12-12 11:41:06 -0500

    Update main.js

--- main.js
+++ main.js
@@ -24,6 +24,6 @@ var Item = function(name, desc, effect, drop){
 var player = new Player(225, 5, 5, 5);
 // Enemies
 var slime = new Enemy(35, 2, 2, 2);
-function startGame(); {
+function startGame() {
 var plname = prompt("What's your name pal?");
 document.write('Oh, hello '+name);
