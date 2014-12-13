91f10206f3955eeac4ff9f27a3133166b24e9dc9
Antwaun Tune <hearkofsorcerey@gmail.com>
2014-12-12 20:20:03 -0500

    Updated all files

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ function startGame() {
   var cant_walk = false;
   var in_battle = false;
 var plname = prompt("What's your name pal?");
-var msg = document.createElement("div");  // Creates a new <div> node
-msg.textContent = "Hello " +plname;         // Sets the text content
-document.body.appendChild(msg);  
+var x = document.createElement("div");  // Creates a new <div> node
+x.textContent = "Hello " +plname;         // Sets the text content
+document.body.appendChild(x);  
 function walk() {
   var num = Math.floor((Math.random() * 10) + 1);
   var num_2 = Math.floor((Math.random() * 10) + 1);
