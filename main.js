86d9bb3e5b66a515d25d5a41e88f44b326884b38
Heark <hearkofsorcerey@gmail.com>
2014-12-12 19:50:03 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 // Main
 // Player
 function startGame() {
+  var cant_walk = false;
+  var in_battle = false;
 var plname = prompt("What's your name pal?");
 document.write('Oh, hello '+name);
+document.write('<b>Welcome to RPGTB</b>');
+function walk() {
+  var num = Math.floor((Math.random() * 10) + 1);
+  var num_2 = Math.floor((Math.random() * 10) + 1);
+  if (num => 5){
+    in_battle = true;
+    document.write('Battle Found!');
+    cant_walk = true;
+  } else if (num <= 5){
+    document.write('Nothing');
+  }
+}
 }
