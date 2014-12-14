c1abb8d57a0a74d8d2ba2087bbe3c5b24262f851
Heark <hearkofsorcerey@gmail.com>
2014-12-14 08:59:33 -0500

    Update enemies.js

--- enemies.js
+++ enemies.js
@@ -1,3 +1,12 @@
 // Enemies
 
 var slime = new Enemy("Slime", 35, 2, 2, 2);
+var bat = new Enemy('Bat', 65, 6, 6, 6);
+
+// selected monster
+var selectedmonster;
+if (num_2 < 5){
+  selectedmonster = slime;
+} else if (num_2 > 5){
+  selectedmonster = bat;
+}
