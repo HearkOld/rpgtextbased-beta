3e3415e5f626feb978d16b50bc0dd979ca4c0992
Heark <hearkofsorcerey@gmail.com>
2014-12-16 10:46:25 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,37 +2,25 @@
 function startGame() {
   var cant_walk = false;
   var in_battle = false;
-var x = document.createElement("div");
+var plname = prompt("What's your name pal?");
+var x = document.createElement("div");  
 x.textContent = "Hello " +plname;         
 document.body.appendChild(x);  
-function walk() { if (cant_walk = false){
+function walk() {
+  if (cant_walk = false){
   var num = Math.floor((Math.random() * 10) + 1);
   var num_2 = Math.floor((Math.random() * 10) + 1);
   if (num >= 5){
     in_battle = true;
-        x.textContent = "Battle Found!"         
+    x.textContent = "Battle found";         
 document.body.appendChild(x);  
     cant_walk = true;
   } else if (num <= 5){
-       x.textContent = "Nothing";         
+   x.textContent = "Nothing";         
 document.body.appendChild(x);  
   }
-} else if (cant_walk = true){
-  x.textContent = "You cannot walk now!";       
-document.body.appendChild(x);  
-}
-function attack() {
-  if (in_battle = true){
-    x.textContent = ""+plname+" attacked the " +selectedmonster.name;         
-document.body.appendChild(x);  
-  if (selectedmonster.health <= 0){
-    x.textContent = ""+plname+" killed " +selectedmonster.name;         
-document.body.appendChild(x);  
-  } else if (selectedmonster.health > 0){
-        x.textContent = ""+selectedmonster.name+ " has "+selectedmonster.health+ " hp left";         
-document.body.appendChild(x);  
+  } else if (cant_walk = true){
+    // finish later
   }
-}
-}
 }
 }
