c7aa222f2d94f99a0dc24049b11c8a8e1a35e65a
Heark <hearkofsorcerey@gmail.com>
2015-07-06 19:43:00 -0400

    Update main.js

--- main.js
+++ main.js
@@ -6,21 +6,4 @@ var plname = prompt("What's your name pal?");
 var x = document.createElement("div");  
 x.textContent = "Hello " +plname;         
 document.body.appendChild(x);  
-function walk() {
-  if (cant_walk == false){
-  var num = Math.floor((Math.random() * 10) + 1);
-  var num_2 = Math.floor((Math.random() * 10) + 1);
-  if (num >= 5){
-    in_battle = true;
-    x.textContent = "Battle found";         
-document.body.appendChild(x);  
-    cant_walk = true;
-  } else if (num <= 5){
-   x.textContent = "Nothing";         
-document.body.appendChild(x);  
-  }
-  } else if (cant_walk == true){
-    // finish later
-  }
-}
 }
