ce0384b9c0387b36cedd81e5203eb2351efca53e
Heark <hearkofsorcerey@gmail.com>
2015-07-06 20:42:23 -0400

    Update main.js

--- main.js
+++ main.js
@@ -30,16 +30,17 @@ document.body.appendChild(x);
 function walk() {
 if(game_on == true){
 var w = document.createElement("div"); 
-w.textContent = "You walk..."         
+w.textContent = "You walk...";         
 document.body.appendChild(w);
 var num = Math.floor((Math.random() * 200) + 1);
  if (num > 1 && windowsize < 50) {
-  start_battle()
+  start_battle();
 } else if(num > 51 && windowsize < 100){
- w.textContent = "You find nothing."         
+ w.textContent = "You find nothing.";         
 document.body.appendChild(w);
 }  else if(num > 101 && windowsize < 150){
- w.textContent = "You find something."         
+ w.textContent = "You find something.";         
 document.body.appendChild(w);
+}
 }
 }
