75ed504bd4f0edbc3fd5bc642d36980af4e8a297
Heark <hearkofsorcerey@gmail.com>
2015-07-06 20:12:39 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 // Main
   var cant_walk = false;
   var in_battle = false;
+  var game_on;
 function startGame() {
-var game_on = true
+game_on = true
 var plname = prompt("What's your name pal?");
 var x = document.createElement("div");  
 x.textContent = "Hello " +plname;         
