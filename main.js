d5f8608c8d0488683ca8c7aa491058e50cf0abc5
Heark <hearkofsorcerey@gmail.com>
2015-07-06 20:27:29 -0400

    Update main.js

--- main.js
+++ main.js
@@ -11,17 +11,17 @@ document.body.appendChild(x);
   document.getElementsByTagName("head")[0].appendChild(fileref);
 }
 }
-load("main.js", "js");
-load("characters.js", "js");
-load("classes.js", "js");
-load("enemies.js", "js");
-load("mob.js", "js");
 
   var cant_walk = false;
   var in_battle = false;
   var game_on;
 function startGame() {
 game_on = true;
+load("main.js", "js");
+load("characters.js", "js");
+load("classes.js", "js");
+load("enemies.js", "js");
+load("mob.js", "js");
 var plname = prompt("What's your name pal?");
 var x = document.createElement("div");  
 x.textContent = "Hello " +plname;         
