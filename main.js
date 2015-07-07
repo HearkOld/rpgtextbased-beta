6720c91d0a645ba6e396d771fd5106db02505737
Heark <hearkofsorcerey@gmail.com>
2015-07-06 20:03:42 -0400

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var plname = prompt("What's your name pal?");
 var x = document.createElement("div");  
 x.textContent = "Hello " +plname;         
 document.body.appendChild(x);  
-}
+},
 function walk() {
 if(game_on == true){
   x.textContent = "Player walks...";         
